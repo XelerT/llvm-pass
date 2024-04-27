e2af64108d2523857d5f2f433398329587d518f3
Alexander.Taranov <taranov.ad@phystech.edu>
2024-04-28 00:06:01 +0300

    Added example.

---
+++ test/data/sources/ex2.cpp
@@ -0,0 +1,9 @@
+int code2 (int a)
+{
+        int c = 5;
+        int b = 10;
+        int v = 15;
+        int d = 20;
+        d = a + b + c + v + d;
+        return a;
+}
---
+++ test/data/sources/ex3.cpp
@@ -0,0 +1,12 @@
+int code3 (int a)
+{
+        int c = 5;
+        int b = 10;
+        int v = 15;
+        int d = 20;
+        d = a + b + c + v + d;
+        return a;
+        
+        a *= 2;
+        c = c * c * c;
+}
