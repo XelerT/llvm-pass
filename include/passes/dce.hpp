e6c474af79a4698701c5e70664ce8e93adba4c2c
Alexander.Taranov <taranov.ad@phystech.edu>
2024-05-02 11:33:32 +0300

    Refactored dce.
    - Removed set.
    - Added instruction attribute check.
    - Removed container referance in delete_instruction.

--- include/passes/dce.hpp
+++ include/passes/dce.hpp
@@ -1,5 +1,6 @@
 #pragma once 
 
+#include "llvm/Transforms/Utils/Local.h"
 #include "llvm/IR/PassManager.h"
 
 namespace my_llvm
@@ -11,12 +12,39 @@ class dce_pass_t : public llvm::PassInfoMixin<dce_pass_t>
                 llvm::PreservedAnalyses run (llvm::Function &func, 
                                              llvm::FunctionAnalysisManager &func_an_man);
         private:
+                template <typename Container>
                 bool delete_instruction
-                (llvm::Instruction *instr, std::set<llvm::Instruction*> &next2delete, 
+                (llvm::Instruction *instr, std::back_insert_iterator<Container> next2delete, 
                  llvm::TargetLibraryInfo &info);
 
                 bool elim_dead_code 
                 (llvm::Function &func, llvm::TargetLibraryInfo &info);
 };
+
+template <typename Container>
+inline bool dce_pass_t::delete_instruction 
+(llvm::Instruction *instr, std::back_insert_iterator<Container> next2delete, 
+ llvm::TargetLibraryInfo &info)
+{
+        if (llvm::isInstructionTriviallyDead(instr, &info)) {
+                unsigned char end_indx = instr->getNumOperands();
+                for (unsigned char i = 0; i != end_indx; ++i) {
+                        llvm::Value *operand = instr->getOperand(i);
+                        instr->setOperand(i, nullptr);
+                        
+                        if (!operand->use_empty() || instr == operand) // check if we need to delete uses
+                                continue;
+                
+                        if (auto operand_instr = llvm::dyn_cast<llvm::Instruction>(operand)) // add dead instructions to delete them further
+                                if (llvm::isInstructionTriviallyDead(operand_instr, &info)) {
+                                        operand_instr->setHasNoInfs(true);
+                                        next2delete = operand_instr;
+                                }
+                }
+                instr->eraseFromParent();
+                return true;
+        }
+        return false;
+} 
 
 }
--- src/passes/dce.cpp
+++ src/passes/dce.cpp
@@ -1,57 +1,33 @@
-#include <set>
 #include <iostream>
+#include <vector>
+
 #include  "llvm/Analysis/TargetLibraryInfo.h"
 #include "llvm/IR/InstIterator.h"
 #include "llvm/Pass.h"
 #include "llvm/Transforms/Scalar.h"
 #include "llvm/Transforms/Utils/AssumeBundleBuilder.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
-#include "llvm/Transforms/Utils/Local.h"
+#include "llvm/IR/Attributes.h"
 
 #include "dce.hpp"
 
 namespace my_llvm
 {
 
-bool dce_pass_t::delete_instruction 
-(llvm::Instruction *instr, std::set<llvm::Instruction*> &next2delete, 
- llvm::TargetLibraryInfo &info)
-{
-        if (llvm::isInstructionTriviallyDead(instr, &info)) {
-                unsigned char end_indx = instr->getNumOperands();
-                for (unsigned char i = 0; i != end_indx; ++i) {
-                        llvm::Value *operand = instr->getOperand(i);
-                        instr->setOperand(i, nullptr);
-                        
-                        if (!operand->use_empty() || instr == operand) // check if we need to delete uses
-                                continue;
-                
-                        if (auto operand_instr = llvm::dyn_cast<llvm::Instruction>(operand)) // add dead instructions to delete them further
-                                if (llvm::isInstructionTriviallyDead(operand_instr, &info))
-                                        next2delete.insert(operand_instr);
-                }
-                instr->eraseFromParent();
-                return true;
-        }
-        return false;
-} 
-
 bool dce_pass_t::elim_dead_code (llvm::Function &func, llvm::TargetLibraryInfo &info)
 {
         bool changed = false;
-        std::set<llvm::Instruction*> next2delete;
+        std::vector<llvm::Instruction*> next2delete;
 
         for (llvm::Instruction &instr : llvm::make_early_inc_range(llvm::instructions(func))) {
-                if (!next2delete.count(&instr))
-                        changed |= delete_instruction(&instr, next2delete, info);
+                if (!instr.hasNoInfs())
+                        changed |= delete_instruction(&instr, std::back_inserter(next2delete), info);
         }
 
         while (!next2delete.empty()) {
-                auto it = next2delete.end();
-                it--;
-                auto instr = *it;
-                next2delete.erase(it);
-                changed |= delete_instruction(instr, next2delete, info);
+                auto instr = next2delete.back();
+                next2delete.pop_back();
+                changed |= delete_instruction(instr,  std::back_inserter(next2delete), info);
         }
 
         return changed;
